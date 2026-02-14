da860c39c80a95eb6def19c5df197d962765d8c6
Swayam Yadav <swayam.science777@gmail.com>
2026-02-14 00:54:41 +0000

    Day 2: Added HTTP server with /health and /status endpoints

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
 
-func main(){
-	fmt.Println("Server Starting .....")
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Health endpoint hit")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }
+
+func statusHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Status endpoint hit")
+	w.Header().Set("Content-Type", "application/json")
+
+	response := map[string]string{
+		"status": "running",
+	}
+
+	json.NewEncoder(w).Encode(response)
+}
+
+func main() {
+	http.HandleFunc("/health", healthHandler)
+	http.HandleFunc("/status", statusHandler)
+
+	log.Println("Starting server on port 8080...")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
